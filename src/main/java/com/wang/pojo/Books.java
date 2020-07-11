7ff00391ec65f5ace8589ee3dc7a23237011bfb9
huayang wang <yunlong.jia@stardust.ai>
2020-07-12 00:01:11 +0800

    图书管理SSM配置完成

---
+++ src/main/java/com/wang/pojo/Books.java
@@ -0,0 +1,16 @@
+package com.wang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Books {
+    private int BookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+}
